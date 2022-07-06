37ba95a95508ac2302e02a38a9efd2777bd59bf8
nerdycryptoboi <divyeshlalwani13@gmail.com>
2022-07-06 14:40:12 +0530

    project initialized

---
+++ server.js
@@ -0,0 +1,25 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index.js");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.use("/", indexRouter);
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to mongoose"));
+
+app.listen(process.env.PORT || 3000);
